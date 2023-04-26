73c355bdabbe89d6c56abeff2d9464e3d08d5259
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-04-27 01:05:32 +0530

    feat: initial commit

---
+++ Crypto-iOS/Crypto_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_iOSApp.swift
+//  Crypto-iOS
+//
+//  Created by Prashanna Rajbhandari on 27/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
