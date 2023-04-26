e8960ba76794bf94cedb3cc464c12769c298234f
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-04-27 01:21:33 +0530

    feat: project theme and asstes setup

--- Crypto-iOS/ContentView.swift
+++ Crypto-iOS/ContentView.swift
@@ -9,11 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack{
+                Text("Accet Color")
+                    .foregroundColor(Color.theme.accent)
+                Text("Sec Color")
+                    .foregroundColor(Color.theme.secondaryText)
+                Text("Red Color")
+                    .foregroundColor(Color.theme.red)
+                Text("Green Color")
+                    .foregroundColor(Color.theme.green)
+            }
         }
         .padding()
     }
