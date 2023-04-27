63c37195bd0b35099ad31f224338789c67395ab9
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-04-28 00:18:27 +0530

    feat: header design implemented

---
+++ Core/Home/View/CircleButtonAnimation.swift
@@ -0,0 +1,20 @@
+//
+//  CircleButtonAnimation.swift
+//  Crypto-iOS
+//
+//  Created by Prashanna Rajbhandari on 28/04/2023.
+//
+
+import SwiftUI
+
+struct CircleButtonAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleButtonAnimation_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleButtonAnimation()
+    }
+}
---
+++ Core/Home/View/HomeView.swift
@@ -0,0 +1,58 @@
+//
+//  HomeView.swift
+//  Crypto-iOS
+//
+//  Created by Prashanna Rajbhandari on 27/04/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showPortfolio : Bool = false
+    
+    
+    var body: some View {
+        ZStack{
+            //background
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            //content
+            VStack{
+                HStack{
+                    CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                        .animation(nil, value: UUID())
+                    Spacer()
+                    Text(showPortfolio ? "Portfolio" : "Live Prices")
+                        .animation(nil, value: UUID())
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.theme.accent)
+                    Spacer()
+                    CircleButtonView(iconName: "chevron.right")
+                        .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                        .onTapGesture {
+                            withAnimation(.spring()){
+                                showPortfolio.toggle()
+                            }
+                        }
+                    
+                }
+                .padding(.horizontal)
+                .padding(.horizontal)
+                
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            HomeView()
+                .toolbar(.hidden, for: .navigationBar)
+        }
+    }
+}
