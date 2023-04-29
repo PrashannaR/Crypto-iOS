f7bf48a1abe3b174d90310771068f76895a70ad3
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-04-29 19:13:06 +0530

    feat: customs errors added

--- Crypto-iOS/Utilities/NetworkingManager.swift
+++ Crypto-iOS/Utilities/NetworkingManager.swift
@@ -10,6 +10,22 @@ import Foundation
 import Combine
 
 class NetworkingManager {
+    
+    enum NetworkingError: LocalizedError{
+        case badURLResponse
+        case unknown
+        
+        var errorDescription: String?{
+            switch self{
+            case .badURLResponse:
+                return "[🔥] Bad response from the URL"
+            case .unknown:
+                return "[⚠️] Unknown error occurred"
+            }
+        }
+    }
+    
+    
     static func download(url: URL) -> AnyPublisher<Data, any Error> {
         return URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
@@ -21,7 +37,7 @@ class NetworkingManager {
     static func handleURLResponse(output: URLSession.DataTaskPublisher.Output) throws -> Data {
         guard let response = output.response as? HTTPURLResponse,
               response.statusCode >= 200 && response.statusCode < 300 else {
-            throw URLError(.badServerResponse)
+            throw NetworkingError.badURLResponse
         }
 
         return output.data
