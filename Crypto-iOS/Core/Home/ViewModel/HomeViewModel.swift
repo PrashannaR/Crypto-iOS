44b2c7937112edf6cc1a518be1e554c9ea5d3cb1
Prashanna Rajbhandari <prashanna.rajbhandari1@gmail.com>
2023-05-02 15:11:47 +0530

    feat: search feature implemented

--- Crypto-iOS/Core/Home/ViewModel/HomeViewModel.swift
+++ Crypto-iOS/Core/Home/ViewModel/HomeViewModel.swift
@@ -5,27 +5,49 @@
 //  Created by Prashanna Rajbhandari on 29/04/2023.
 //
 
-import Foundation
 import Combine
+import Foundation
 
-class HomeViewModel: ObservableObject{
-    @Published var allCoins : [CoinModel] = []
-    @Published var portfolioCoins : [CoinModel] = []
-    @Published var searchBarText : String = ""
+class HomeViewModel: ObservableObject {
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
     
+    @Published var searchBarText: String = ""
+
     private let dataService = CoinDataService()
-    
+
     private var cancellables = Set<AnyCancellable>()
-    
-    init(){
+
+    init() {
         addSubscriber()
     }
-    
-    func addSubscriber(){
-        dataService.$allCoins
+
+    func addSubscriber() {
+        
+        //updates allCoins
+        $searchBarText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.2), scheduler: DispatchQueue.main)
+            .map(filteredCoins)
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+    }
+    
+    private func filteredCoins(text: String, coins:[CoinModel]) -> [CoinModel]{
+        guard !text.isEmpty else {
+            return coins
+        }
+
+        let lowercasedText = text.lowercased()
+
+        let filteredCoins = coins.filter { coin -> Bool in
+            coin.name.lowercased().contains(lowercasedText) ||
+                coin.symbol.lowercased().contains(lowercasedText) ||
+                coin.id.lowercased().contains(lowercasedText)
+        }
+
+        return filteredCoins
     }
 }
